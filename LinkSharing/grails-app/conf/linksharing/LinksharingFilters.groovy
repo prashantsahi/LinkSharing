5c5eccf4d9c40c26171fbcc8eac0bf01d304d540
Prashant Sahi <prashant.sahi@intelligrape.com>
2015-03-17 11:19:34 +0530

    ignore the user controllers showImage action in before filter

--- LinkSharing/grails-app/conf/linksharing/LinksharingFilters.groovy
+++ LinkSharing/grails-app/conf/linksharing/LinksharingFilters.groovy
@@ -5,6 +5,12 @@ class LinksharingFilters {
     def filters = {
         restrictLogin(controller: 'login|assets', action: 'loginHandler|index|register', invert: true) {
             before = {
+                println params
+                if(params.controller=='user'&&params.action=='showImage')
+                {
+                    println 'inside if++++++++++++++++++++++++++++++++++'
+                    return
+                }
                 if (!session['username']) {
                     redirect(controller: 'login', action: 'index')
                     flash.message = "Please log-in to the system"
